c470d6d981a4eafa6bc66b3b225e79fad757f949
fbmxusdt <fbmxusdt@gmail.com>
2026-05-18 22:03:03 +0800

    Updates

--- assets/all-wallets-C1jOZ9NO.js
+++
@@ -1,6 +0,0 @@
-import{w as l}from"./core-DQxdkeci.js";import"./index-a5q0SNvo.js";import"./index.es-Dn3I3U3x.js";const i=l`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{i as allWalletsSvg};

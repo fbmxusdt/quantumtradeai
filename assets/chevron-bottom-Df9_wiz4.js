5906e5f878430c10d3e9073c3111033194774a22
fbmxusdt <fbmxusdt@gmail.com>
2026-05-20 15:36:48 +0800

    Updates

--- assets/chevron-bottom-Df9_wiz4.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-BuYQJW52.js";import"./index-CbmEzdQX.js";import"./index.es-DA7isjtw.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronBottomSvg};
